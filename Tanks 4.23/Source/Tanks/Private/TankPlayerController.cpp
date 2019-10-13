b03f35e4a256c803df08c7d3d2982c14cb9b320e
Aris K <ariskyr@gmail.com>
2019-10-13 20:53:51 +0300

    Bt26 Finding AimDot Screen pixel coordinates

--- Tanks 4.23/Source/Tanks/Private/TankPlayerController.cpp
+++ Tanks 4.23/Source/Tanks/Private/TankPlayerController.cpp
@@ -38,7 +38,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 	FVector OutHitLocation; // Out parameter
 	if (GetSightRayHitLocation(OutHitLocation))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *OutHitLocation.ToString())
+		//UE_LOG(LogTemp, Warning, TEXT("Look coords: %s"), *OutHitLocation.ToString())
 
 			//if it hits the landscape
 			// TODO tell controlled tank to aim at this point
@@ -48,6 +48,13 @@ void ATankPlayerController::AimTowardsCrosshair()
 // Get world location of linetrace through crosshair, true if hit landscape
 bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
 {
-	OutHitLocation = FVector(1.0);
-	return true;
+	// Find crosshair position
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrosshairXLocation, ViewportSizeY * CrosshairYLocation);
+	UE_LOG(LogTemp, Warning, TEXT("Screen Location: %s"), *ScreenLocation.ToString());
+
+	// Deproject the screen position of the crosshair to a world direction
+	// Line trace along that look direction and see what we hit ( up to max range )
+	return true	;
 }
--- Tanks 4.23/Source/Tanks/Public/TankPlayerController.h
+++ Tanks 4.23/Source/Tanks/Public/TankPlayerController.h
@@ -27,4 +27,9 @@ private:
 
 	// return an OUT parameter, true if hit location
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5;
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.3333;
 };
